841cfb41ebef1087be83bcb3f8fdd508d5c1948f
Heroblender <pinnekemoes@gmail.com>
2024-10-29 20:31:41 +0100

    Lab_03_hidevowels

--- EX03_Lab/practice_string_functions.py
+++ EX03_Lab/practice_string_functions.py
@@ -12,3 +12,14 @@ def proper_caps(txt):
 
 inpt = input("Enter a movie name: ")
 print(f"* WATCH NOW * {proper_caps(inpt)}")
+
+
+def hide_vowels(text):
+    vowels = "aeiouAEIOU"
+    for vowel in vowels:
+        text = text.replace(vowel, "*")
+    return text
+
+inpt = input("Enter a word to guess: ")
+print("* This word is secret:",hide_vowels(inpt))
+print(".\n.\n.\n.\n* psst.. don't tell anyone.. the actual word is", inpt)
