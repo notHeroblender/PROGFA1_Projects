706db7924f33bf10dd1e7a5781e0ee8bb2f384d3
Heroblender <pinnekemoes@gmail.com>
2024-11-04 22:01:05 +0100

    Lab_03_betterFilenameGenerator

--- EX01_variables/filename_generator.py
+++ EX01_variables/filename_generator.py
@@ -3,4 +3,17 @@ first_name = str(input("What is your first name? "))
 group = int(input("What is your group number? "))
 lab_nr = int(input("Which lab is this? "))
 
-print(f"1DAE{group:02d}_{last_name}_{first_name}_L{lab_nr:02d}")
+def clean_string(toclean:str)->str:
+    """
+    cleans off spaces, inner spaces and dashes replaced with underscore, apostrophes deleted
+    :return:
+    """
+    toclean.strip()
+    toclean = toclean.replace(" ","_")
+    toclean = toclean.replace("-","_")
+    toclean = toclean.replace("'","")
+
+    return toclean
+
+
+print(f"1DAE{group:02d}_{clean_string(last_name)}_{clean_string(first_name)}_L{lab_nr:02d}")
