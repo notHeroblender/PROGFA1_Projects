f5e06257879009b3b2b87cc2cc41a5e7dab1a86e
Heroblender <pinnekemoes@gmail.com>
2024-11-04 20:29:22 +0100

    Lab_03_stringfunctions

--- EX03_Lab/practice_string_functions.py
+++ EX03_Lab/practice_string_functions.py
@@ -23,3 +23,37 @@ def hide_vowels(text):
 inpt = input("Enter a word to guess: ")
 print("* This word is secret:",hide_vowels(inpt))
 print(".\n.\n.\n.\n* psst.. don't tell anyone.. the actual word is", inpt)
+
+
+def hangman(letter:str="")-> tuple:
+    """
+    lets the user guess a letter in a hidden word
+    :param letter: user's guess
+    :return: a tuple with 4 values/ 0. the hidden word as a string/ 1. the number of characters the given letter appears in the word/ 2. the first index of the letter in the word/ 3. the last index of the letter in the word
+    """
+    word = "occurrence"
+    hidden = "*" * len(word)
+
+    amount = word.count(letter)
+    first = word.find(letter)
+    last = word.rfind(letter)
+
+    return hidden,amount,first,last
+
+inpt = input(f"Please enter a single character you want to find in my hidden word {hangman()[0]}: ")
+print("* NUMBER OF TIMES THE CHARACTER IS IN THIS WORD:",hangman(inpt)[1])
+print("* FIRST INDEX OF THE CHARACTER IN THIS WORD:",hangman(inpt)[2])
+print("* LAST INDEX OF THE CHARACTER IN THIS WORD:",hangman(inpt)[3])
+
+
+def filename_decoder(filename:str):
+    first = filename.split("_")[1]
+    last = filename.split("_")[2]
+    group = filename.split("_")[0]
+
+    return group,first,last
+
+inpt = input("Enter a filename of your project(group_lastname_firstname): ")
+print("* FIRST NAME:", filename_decoder(inpt)[2])
+print("* LAST NAME:", filename_decoder(inpt)[1])
+print("* GROUP:", filename_decoder(inpt)[0])
