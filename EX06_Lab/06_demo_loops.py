6076bd1434b033ea81ea796680ed9455485ef46b
Heroblender <pinnekemoes@gmail.com>
2024-11-15 09:58:25 +0100

    Create 06_demo_loops.py

---
+++ EX06_Lab/06_demo_loops.py
@@ -0,0 +1,9 @@
+nr = 3
+counter = 0
+
+#for count in range(1,11):
+#    print(f"{nr} * {count} = {count*nr}")
+
+while counter < 30:
+    counter += 1
+    print(f"{nr} * {counter} = {counter * nr}")
