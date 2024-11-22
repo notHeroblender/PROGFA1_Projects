78ad664af64788be126683f65cbb3b0c1a8c7785
Heroblender <pinnekemoes@gmail.com>
2024-11-22 11:42:24 +0100

    07_demo_sus Added Resources and bad code

--- EX07_Lab/07_demo_sus.py
+++ EX07_Lab/07_demo_sus.py
@@ -13,11 +13,22 @@ import math
 from pygame.math import Vector2
 
 # Create an instance of ProgfaEngine and set window size (width, height):
-engine = pfe.ProgfaEngine(800, 600)
+engine = pfe.ProgfaEngine(300, 300)
 
 # Set the frame rate to x frames per second:
 engine.set_fps(60)
 
+#wrong way
+frame_01 = engine.load_image("Resources/crewmates/green1.png")
+frame_02 = engine.load_image("Resources/crewmates/green2.png")
+frame_03 = engine.load_image("Resources/crewmates/green3.png")
+frame_04 = engine.load_image("Resources/crewmates/green4.png")
+
+frames = frame_01, frame_02, frame_03, frame_04
+
+current_frame = 1
+timer = 0
+max_timer = 10
 
 def setup():
     """
@@ -31,7 +42,35 @@ def render():
     """
     This function is being executed over and over, as fast as the frame rate. Use to draw (not update).
     """
+    global current_frame
+    engine.background_color = 1,1,1
+    
+    draw_frame(current_frame)
 
+    pass
+
+def draw_frame(frame:int=0):
+    """
+    draw a crewmate image based on frame
+    :param frame:
+    :return:
+    """
+    #Wrong way
+    if frame == 0:
+        frame_01.draw(0,0)
+    if frame == 1:
+        frame_02.draw(0,0)
+    if frame == 2:
+        frame_03.draw(0,0)
+    if frame == 3:
+        frame_04.draw(0,0)
+
+    #Debug
+    engine.set_font_size(20)
+    engine.color = 0,0,0
+    engine.draw_text(str(frame),20,20)
+
+    #frames[frame].draw(0,0)
     pass
 
 
@@ -39,6 +78,14 @@ def evaluate():
     """
     This function is being executed over and over, as fast as the frame rate. Use to update (not draw).
     """
+    #Wrong way
+    global current_frame, timer
+    timer += 1
+    if timer >= max_timer:
+        timer = 0
+        current_frame += 1
+        if current_frame > 3:
+            current_frame = 0
 
     pass
 
@@ -47,7 +94,6 @@ def mouse_pressed_event(mouse_x: int, mouse_y: int, mouse_button: MouseButton):
     """
     This function is only executed once each time a mouse button was pressed!
     """
-
     pass
 
 
