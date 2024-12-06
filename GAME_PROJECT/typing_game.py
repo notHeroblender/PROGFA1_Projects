d051c14e65d385788e6dbe0af3721b36283663bd
Heroblender <pinnekemoes@gmail.com>
2024-12-06 10:09:17 +0100

    typing_game display random word

--- GAME_PROJECT/typing_game.py
+++ GAME_PROJECT/typing_game.py
@@ -13,6 +13,7 @@ import math
 
 from dae_progfa_lib.progfa_image import ProgfaImage
 from pygame.math import Vector2
+import random
 
 # Create an instance of ProgfaEngine and set window size (width, height):
 engine = pfe.ProgfaEngine(960, 540)
@@ -20,26 +21,43 @@ engine = pfe.ProgfaEngine(960, 540)
 # Set the frame rate to x frames per second:
 engine.set_fps(60)
 
-
+#GMEPLAY
+#Visuals
 layers = []
 layer_amt = 3
-
 player_img:ProgfaImage
-
 list_x_pos = []
+#Logic
+displayed_word = ""
+current_words = []
+easy_words = ["cat", "dog", "book", "tree", "star", "fish", "blue", "apple", "chair", "house", "train",
+              "light", "happy", "mouse", "plane", "smile", "paper", "cloud", "water", "horse"]
+medium_words = []
+hard_words = []
+
 
 def setup():
     """
     Only executed ONCE (at the start); use to load files and initialize.
     """
-    global player_img
+    init_gameplay()
+
+    pass
+
+def init_gameplay():
+    global player_img, words
 
     for i in range(layer_amt):
-        layers.append(engine.load_image(f"Resources/{i+1}.png"))
-    #print(layers)
+        layers.append(engine.load_image(f"Resources/{i + 1}.png"))
+    # print(layers)
 
     player_img = engine.load_image("Resources/player.png")
     print(player_img)
+
+    #set difficulty
+    current_words.clear()
+    current_words.extend(easy_words)
+
     pass
 
 
@@ -47,21 +65,50 @@ def render():
     """
     This function is being executed over and over, as fast as the frame rate. Use to draw (not update).
     """
+
+    #if Current_State = GameState.PLAY:
+    draw_gameplay()
+
+    pass
+
+def draw_gameplay():
+    #draw_visuals()
+    draw_game_text()
+
+    pass
+
+def draw_visuals():
+    """
+    draws the visuals (background and player)
+    :return:
+    """
     global player_img
 
-    engine.background_color = (0.011764705882352941,0.1568627450980392,0.12549019607843137)
+    engine.background_color = (0.011764705882352941, 0.1568627450980392, 0.12549019607843137)
 
-    for i in range(len(layers)):    #set every layer's x pos to 0
-        list_x_pos.append(0)    #add one at 0
-        list_x_pos.append(engine.width) #add one just off-screen
+    for i in range(len(layers)):  # set every layer's x pos to 0
+        list_x_pos.append(0)  # add one at 0
+        list_x_pos.append(engine.width)  # add one just off-screen
 
-    for i in range(len(layers)):    #draw every layer
-        layers[i].draw(list_x_pos[2*i],0)   #0,2,4...
-        layers[i].draw(list_x_pos[2*i+1],0)   #1,3,5...
+    for i in range(len(layers)):  # draw every layer
+        layers[i].draw(list_x_pos[2 * i], 0)  # 0,2,4...
+        layers[i].draw(list_x_pos[2 * i + 1], 0)  # 1,3,5...
 
     animate_parallax(1)
 
-    player_img.draw(0,0)
+    player_img.draw(0, 0)
+
+    pass
+
+def draw_game_text():
+    global current_words,displayed_word
+
+    #temp
+    engine.background_color = 0,0,0
+
+    engine.set_font_size(20)
+    engine.color = 1,1,1
+    engine.draw_text(displayed_word, engine.width/2,engine.height/2, True)
 
     pass
 
@@ -110,6 +157,10 @@ def key_up_event(key: str):
     This function is only executed once each time a key was released!
     Special keys have more than 1 character, for example ESCAPE, BACKSPACE, ENTER, ...
     """
+    global current_words, displayed_word
+
+    if key == " ":
+        displayed_word = random.choice(current_words)
 
     pass
 
